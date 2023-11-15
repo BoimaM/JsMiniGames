d3190b68d48a4242c524240cb0b5f36e2942c218
BoimaM <MasfortBoima@gmail.com>
2023-11-14 23:03:47 -0500

    Update RandomGuess.js

--- RandomGuess.js
+++ RandomGuess.js
@@ -1,15 +1,23 @@
 const prompt = require ("prompt-sync")();
 
 const target = Math.round(Math.random() * 100)
-console.log(target);
+
 
 let guesses  = 0;
 
-const guess = Number (prompt ("Guess the number (0-100): "));
 
-if (guess > target) {
-  console.log("Your guess is too high.");
-}
-else{
-  console.log("Your guess is too low.");
-}
+while (true) {
+  guesses++
+
+  const guess = Number (prompt ("Guess the number (0-100): "));
+  if (guess > target) {
+    console.log("Your guess is too high.");
+  } else if (guess < target){
+    console.log("Your guess is too low.");
+  } else {
+    console.log("You guessed it");
+    break;
+  }
+};
+
+console.log(`You guessed the number in ${guesses} tries.`);
