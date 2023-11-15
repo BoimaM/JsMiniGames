da0ad3762c54c8f4c98568fbd08c7b7560f28606
BoimaM <MasfortBoima@gmail.com>
2023-11-15 00:12:14 -0500

    Update RockPaperScissor.js

--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -4,27 +4,39 @@ let wins = 0
 let losses = 0
 let ties = 0
 
-const playerchoice = prompt ("Enter rock, paper, or scissors:  ")
+while (true) {
+  const playerChoice = prompt ("Enter rock, paper, or scissors (or q to quit):  ");
+  if (playerChoice.toLowerCase() === "q"){
+    break;
+  };
 
-if (playerchoice !== "rock" && 
-    playerchoice !== "paper" && 
-    playerchoice !== "scissors"){
-    console.log("Not a valid choice, try again")
-}
+  if (playerChoice !== "rock" && 
+      playerChoice !== "paper" && 
+      playerChoice !== "scissors"
+    ){
+      console.log("Not a valid choice, try again");
+      continue;
+  };
 
-const choices =["rock", "paper","scissors"]
-const randomIndex = Math.round(Math.random () * 2)
-const computerChoice = choices[randomIndex];
-console.log(computerChoice);
+  const choices =["rock", "paper","scissors"];
+  const randomIndex = Math.round(Math.random () * 2);
+  const computerChoice = choices[randomIndex];
 
-if (computerChoice === playerchoice){
-  console.log(draw);
-  ties++
-} else if (
-  (playerchoice === "paper" && computerChoice === 'rock') ||  (playerchoice === "rock" && computerChoice === 'scissors') || (playerchoice === "scissors" && computerChoice === 'paper')){
-  console.log('You win!');
-  wins++
-} else {
-  console.log('You lost...');
-  losses++
-}
+  console.log(`The computer chose ${computerChoice}.`);
+
+  if (computerChoice === playerChoice){
+    console.log("You both tied");
+    ties++;
+
+  } else if (
+    (playerChoice === "paper" && computerChoice === 'rock') ||  (playerChoice === "rock" && computerChoice === 'scissors') || (playerChoice === "scissors" && computerChoice === 'paper')){
+    console.log('You win!');
+    wins++;
+
+  } else {
+    console.log('You lost...');
+    losses++;
+  }
+};
+
+console.log(`Game record is = Wins: ${wins}, Losses: ${losses}, Ties: ${ties}`);
