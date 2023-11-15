3a1c2c13e8e1be2347d363fcc53dab7379fc97c1
BoimaM <MasfortBoima@gmail.com>
2023-11-14 23:09:40 -0500

    Update RandomGuess.js

--- RandomGuess.js
+++ RandomGuess.js
@@ -10,14 +10,16 @@ while (true) {
   guesses++
 
   const guess = Number (prompt ("Guess the number (0-100): "));
+  
   if (guess > target) {
     console.log("Your guess is too high.");
+    continue;
   } else if (guess < target){
     console.log("Your guess is too low.");
-  } else {
+    continue;
+  } 
     console.log("You guessed it");
     break;
-  }
 };
 
 console.log(`You guessed the number in ${guesses} tries.`);
