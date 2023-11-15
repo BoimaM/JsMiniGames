5599b4c020538ebb698ef121381cc36c32b7d309
BoimaM <MasfortBoima@gmail.com>
2023-11-15 09:08:07 -0500

    Update RockPaperScissor.js

--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -5,14 +5,14 @@ let losses = 0
 let ties = 0
 
 while (true) {
-  const playerChoice = prompt ("Enter rock, paper, or scissors (or q to quit):  ");
-  if (playerChoice.toLowerCase() === "q"){
+  const userChoice = prompt ("Enter rock, paper, or scissors (or q to quit):  ");
+  if (userChoice.toLowerCase() === "q"){
     break;
   };
 
-  if (playerChoice !== "rock" && 
-      playerChoice !== "paper" && 
-      playerChoice !== "scissors"
+  if (userChoice !== "rock" && 
+      userChoice !== "paper" && 
+      userChoice !== "scissors"
     ){
       console.log("Not a valid choice, try again");
       continue;
@@ -24,12 +24,12 @@ while (true) {
 
   console.log(`The computer chose ${computerChoice}.`);
 
-  if (computerChoice === playerChoice){
+  if (computerChoice === userChoice){
     console.log("You both tied");
     ties++;
 
   } else if (
-    (playerChoice === "paper" && computerChoice === 'rock') ||  (playerChoice === "rock" && computerChoice === 'scissors') || (playerChoice === "scissors" && computerChoice === 'paper')){
+    (userChoice === "paper" && computerChoice === 'rock') ||  (userChoice === "rock" && computerChoice === 'scissors') || (userChoice === "scissors" && computerChoice === 'paper')){
     console.log('You win!');
     wins++;
 
